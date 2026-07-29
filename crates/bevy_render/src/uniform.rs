cfade630a71282bd9a021758d60414147d48532c
Stuart Parmenter <pavlov@pavlov.net>
2026-07-29 16:51:47 -0700

    Label every ComponentUniforms buffer with its component type name

--- crates/bevy_render/src/uniform.rs
+++ crates/bevy_render/src/uniform.rs
@@ -73,11 +73,14 @@ impl<C: Component + ShaderType> ComponentUniforms<C> {
     }
 }
 
-impl<C: Component + ShaderType> Default for ComponentUniforms<C> {
+impl<C: Component + ShaderType + WriteInto> Default for ComponentUniforms<C> {
     fn default() -> Self {
-        Self {
-            uniforms: Default::default(),
-        }
+        let mut uniforms = DynamicUniformBuffer::default();
+        uniforms.set_label(Some(&format!(
+            "{} uniforms buffer",
+            core::any::type_name::<C>()
+        )));
+        Self { uniforms }
     }
 }
 
